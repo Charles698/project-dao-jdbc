7c3adf8731865f379fc4de2a7de113c3385d6494
Charles Felipe <charles.fmsilva@gmail.com>
2025-06-23 12:00:08 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		
 		Seller seller = new Seller(7, "Marcos", "marcos@gmail.com", new Date(), 3000.0, obj);
 		
 		System.out.println(seller);
-		
-		
-
 	}
 }
