d9739ee3757c030f954ac4733dcde6f34a25fcb9
Charles Felipe <charles.fmsilva@gmail.com>
2025-06-24 18:28:03 -0300

    findAll department implementation and final the project

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -18,22 +19,29 @@ public class Program2 {
 		Department dep = depDao.findById(3);
 		System.out.println(dep);
 			
-		System.out.println("\n=== TEST 2: department insert ===");
+		System.out.println("\n=== TEST 2: findAll ===");
+		List<Department> list = depDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== TEST 3: department insert ===");
 		Department newDep = new Department(null, "Automobiles");
 		depDao.insert(newDep);
 		System.out.println("Inserted! New Id = " + newDep.getId());
 		
-		System.out.println("\n=== TEST 3: department update ===");
+		System.out.println("\n=== TEST 4: department update ===");
 		dep = depDao.findById(1);
 		dep.setName("Food");
 		depDao.update(dep);
 		System.out.println("Updated completed!");
 		
-		System.out.println("\n=== TEST 4: department delete ===");
+		System.out.println("\n=== TEST 5: department delete ===");
 		System.out.print("Enter id for delete teste: ");
 		int id = sc.nextInt();
 		depDao.deleteById(id);
 		System.out.println("Deleted completed!");
+		
 		sc.close();
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -127,8 +128,33 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT department.* FROM department ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			
+			
+			while(rs.next()) {
+				
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 	
